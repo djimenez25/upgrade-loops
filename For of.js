a2d0d1fb3ed7e512fe0ee37b181d61c71ef4570a
djimenez25 <daniel.jimenez@bootcamp-upgrade.com>
2022-05-04 13:44:04 +0200

    Iteracion#3

---
+++ For of.js
@@ -0,0 +1,9 @@
+console.log("Archivo for of funcionando OK")
+
+// Usa un bucle forof para recorrer todos los destinos del array. Imprime en un console.log sus valores.
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (const places of placesToTravel) {
+    console.log(places)
+}
